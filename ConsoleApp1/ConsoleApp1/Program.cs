f08e92950f1ccd2b4570bcd63ed868dc0a584e90
jvdev2001 <joaovitoralvess2001@gmail.com>
2026-06-11 17:00:04 -0300

    Finalizacao do primeiro desafio 01

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+
+namespace MyAplication {
+
+    class Program {
+
+        static void Main(string[] args) {
+            Console.WriteLine("1. Leia dois números inteiros e informe qual deles é o maior.");
+
+            int x, y , n;
+
+            Console.WriteLine("Digite primeiro numero inteiro : ");
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo numero :");
+            y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+            {
+                Console.WriteLine($" primeiro numero e maior do que o segundo numero {x}");
+            }
+            else if (y > x)
+            {
+                Console.WriteLine($"Segundo numero e maior do que o primeiro numero {y}");
+            }
+            else {
+                Console.WriteLine($"Numeros {x} e {y} são iguais");
+            }
+           
+
+        }
+    }
+}
