e1dbe744e9f04cfebf9e43d5ba2dd631bb61531d
jvdev2001 <joaovitoralvess2001@gmail.com>
2026-06-11 20:50:11 -0300

    Finalizando o desafio_02

--- Desafio_02/ConsoleApp1/Program.cs
+++ Desafio_02/ConsoleApp1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Desafio02
+{
+    class aplication {
+
+        static void Main(string[] args) {
+
+            Console.WriteLine("Leia dois números e informe se eles são iguais ou diferentes");
+
+            int number1, number2;
+
+            Console.WriteLine("informe o  primeiro numero:");
+            number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("informe o segundo numero:");
+            number2 = int.Parse(Console.ReadLine());
+
+            if (number1 == number2)
+            {
+                Console.WriteLine($"numeros {number1} , {number2} são iguais ");
+            }
+            else if (number1 != number2)
+            {
+                Console.WriteLine($"Numeros {number1} , {number2} são diferentes ");
+            }
+            else {
+                Console.WriteLine("Não digitou numeros");
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
