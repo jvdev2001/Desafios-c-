1603a73bcf6dd64110ecca4e596db2dd4b373667
jvdev2001 <joaovitoralvess2001@gmail.com>
2026-06-12 15:24:18 -0300

    Finalizando o desafio_04

--- Desafio_04/ConsoleApp1/ConsoleApp1/Program.cs
+++ Desafio_04/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Myaplication {
+
+    class Program {
+
+        static void Main(string[] args) {
+            Console.WriteLine("Leia um número e informe se ele é positivo, negativo ou zero.");
+
+            int x;
+            Console.WriteLine("Digite um numero :");
+            x = int.Parse(Console.ReadLine());
+
+            if (x <=0)
+            {
+                Console.WriteLine($"Numero Digitado e negativo {x}");
+            }
+            else {
+                Console.WriteLine($"Numero Digitado e Positivo {x}");
+            }
+
+            Console.ReadLine();
+        }
+    }   
+}
