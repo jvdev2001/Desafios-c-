533e8c4b44fa5bb76e11983bf8c787a3ab2e46f6
jvdev2001 <joaovitoralvess2001@gmail.com>
2026-06-12 15:54:17 -0300

    Finalizacao do Desafio_05

---
+++ Desafio_05/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Myaplication {
+
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Leia a nota de um aluno e informe se ela é maior que 7.");
+
+            double nota;
+
+            Console.WriteLine("Informe a nota do aluno :");
+
+            nota = double.Parse(Console.ReadLine());
+
+            bool maior = nota > 7;
+
+            Console.WriteLine(maior);
+
+            Console.ReadLine();
+        }
+    }    
+}
