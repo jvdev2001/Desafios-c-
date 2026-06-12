d958fcba842f33b9bbb633b6d506e3257f34596b
jvdev2001 <joaovitoralvess2001@gmail.com>
2026-06-12 14:59:19 -0300

    Finalizando o Desafio_03

--- Desafio_03/ConsoleApp1/ConsoleApp1/Program.cs
+++ Desafio_03/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Desafio03 {
+﻿using System.Security.Cryptography;
+
+namespace Desafio03 {
 
     class MyAplication {
 
         static void Main(string[] args) {
 
             Console.WriteLine(". Leia a idade de uma pessoa e informe se ela é maior ou menor de idade");
+
+            int ager;
+
+            Console.WriteLine("Digite sua idade :");
+            ager = int.Parse(Console.ReadLine());
+
+            if (ager >= 18)
+            {
+                Console.WriteLine($"Maior de idade , idade informada {ager}  anos ");
+            }
+            else {
+                Console.WriteLine($"Menor de idade , idade informada {ager} anos ");
+            }
+
+            Console.ReadLine();
         }
     }
 }
