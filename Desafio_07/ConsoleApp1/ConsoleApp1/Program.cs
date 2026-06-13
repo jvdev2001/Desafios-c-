e40360266edfadd072b253c2514b807db941c28b
jvdev2001 <joaovitoralvess2001@gmail.com>
2026-06-13 10:15:50 -0300

    Finalizado o Desafio_07

---
+++ Desafio_07/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Myaplication {
+
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Leia uma idade e informe se a pessoa pode tirar carteira de motorista.");
+
+            int ager;
+
+            int verificaIdadeCarteira = 18;
+
+            Console.WriteLine("Digite idade para a verificacao:");
+            ager = int.Parse(Console.ReadLine());
+
+            if (ager >= verificaIdadeCarteira)
+            {
+                Console.WriteLine($"Cliente esta entre os requisitos para retirar a carteira de motorista : , sua idade e {ager}");
+            }
+            else {
+                Console.WriteLine($"Cliente nao esta entre os requisitos para retirar a carteira de motorista : , sua idade e {ager}");
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
