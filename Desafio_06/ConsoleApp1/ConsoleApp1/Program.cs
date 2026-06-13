9550641a8eb6437f27e36cc80c698dba5392d420
jvdev2001 <joaovitoralvess2001@gmail.com>
2026-06-13 10:01:45 -0300

    finalizado o Desafio_06

---
+++ Desafio_06/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Myaplication {
+        
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine($"Leia três números e informe qual é o maior");
+
+            int x, y, n;
+
+            Console.Write("Digite primeiro numero:");
+            x = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite segundo numero:");
+            y = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite Terceiro numero:");
+            n = int.Parse(Console.ReadLine());
+
+            if (x > y && x>n)
+            {
+                Console.WriteLine($"Primeiro numero e o maior entre os 3 , {x}");
+            }
+            else if (y > x && y>n)
+            {
+                Console.WriteLine($"Segundo numero e maior entre os 3 , {y}");
+            }
+            else{
+                Console.WriteLine($"Terceiro numero e o maior entre os 3 ,{n} ");
+
+            }
+
+                Console.ReadLine();
+
+        }
+    }
+    }
