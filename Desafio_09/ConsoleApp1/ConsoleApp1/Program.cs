54acf7578a2615e1733ddbbf3f9cb2f83e8e0be5
jvdev2001 <joaovitoralvess2001@gmail.com>
2026-06-13 11:36:16 -0300

    Finalizando o desafio_09

---
+++ Desafio_09/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Myaplication {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Leia um número e informe se ele está entre 10 e 50.");
+
+            int number;
+
+            Console.Write("Digite um numero :");
+            number = int.Parse(Console.ReadLine());
+
+            if (number > 10 && number < 50)
+            {
+                Console.WriteLine($"numero Digitado {number} , esta entre 10   e 50");
+            }
+            else {
+                Console.WriteLine($"numero Digitado {number} , não esta entre 10 e 50");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
