fac5e47938f6ed87ed191dae6f8fd98da86abe02
jvdev2001 <joaovitoralvess2001@gmail.com>
2026-06-13 11:19:58 -0300

    Finalizado o Desafio_08

---
+++ Desafio_08/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace MyAplication {
+
+    class Program {
+
+        static void Main(string[] args) {
+            Console.WriteLine("Leia usuário e senha e valide o acesso.");
+
+            string user = "userTest";
+            int passaworld = 0011;
+
+            string validaUser;
+            int validaPassaword;
+
+            Console.WriteLine("Digite o usuario para realiar o login:");
+            validaUser = Console.ReadLine();
+
+            Console.WriteLine("Digite a senha para realizar o login:");
+            validaPassaword = int.Parse(Console.ReadLine());
+
+            if (user == validaUser && passaworld == validaPassaword)
+            {
+                Console.WriteLine("ACESSO LIBERADO");
+            }
+            else {
+                Console.WriteLine("ACESSO NEGADO");
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
