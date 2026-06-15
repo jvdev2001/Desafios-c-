c73cd18005be8373169312cdab272e8547085c25
jvdev2001 <joaovitoralvess2001@gmail.com>
2026-06-15 16:27:35 -0300

    Finalizando o Desafio_10

---
+++ Desafio_10/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MeuAplication {
+
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(" Leia um número e informe se ele é divisível por 2 e por 3");
+
+            int num1;
+
+            Console.WriteLine("Digite um numero inteiro:");
+            num1 = int.Parse(Console.ReadLine());
+
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine("Numero e divisivel por 2");
+            }
+            else {
+                Console.WriteLine("Numero e divisivel por 3");
+            }
+
+            Console.ReadLine();
+        }
+       
+    }
+}
