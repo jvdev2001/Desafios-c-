69bee4784eaeee478c1821b5720c64d345449c31
jvdev2001 <joaovitoralvess2001@gmail.com>
2026-06-16 16:12:21 -0300

    Finalizado o Desafio_12

---
+++ Desafio_12/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace MyApplication{
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(" Calcule e classifique o IMC.");
+
+            double peso, altura, imc;
+
+            Console.Write("informe seu peso:");
+            peso = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.Write("informe sua altura :");
+            altura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            
+            imc = peso /(altura * altura);
+
+            if (imc < 18)
+            {
+                Console.WriteLine($"Magreza , seu imc deu : {imc.ToString("F1")}");
+            }
+            else if (imc >= 18 && imc <= 24)
+            {
+                Console.WriteLine($"Normal, seu imc deu : {imc.ToString("F1")}");
+            }
+            else if(imc>=25 && imc<=29) {
+                Console.WriteLine( $"Sobrepeso , seu imc deu :{imc.ToString("F1")}");
+            }
+
+
+                Console.ReadLine();
+         
+        }
+    }
+}
