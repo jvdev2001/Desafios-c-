b1684e3b4aa98e1097cca97addbcc2dad860751e
jvdev2001 <joaovitoralvess2001@gmail.com>
2026-06-17 16:41:49 -0300

    Finalizando o Desafio_13

--- Desafio_13/ConsoleApp1/ConsoleApp1/Program.cs
+++ Desafio_13/ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,6 +3,30 @@
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Some números até que seja digitado 0.");
+
+            int num , soma;
+
+            soma = 0;
+            
+            Console.WriteLine("Digite um numero:");
+            num = int.Parse(Console.ReadLine());
+
+            while (num > 0) {
+
+                soma = num + soma;
+                Console.WriteLine("Digite um numero:");
+                num = int.Parse(Console.ReadLine());
+
+
+
+            }
+
+           
+
+            Console.Write($"soma dos numeros e {soma}");
+            Console.ReadLine();
+
+
         }
     }
 }
