bae71847d118221eaa46deb206a078741d67cfbe
jvdev2001 <joaovitoralvess2001@gmail.com>
2026-06-17 15:48:24 -0300

    Subindo o Desafio_13

---
+++ Desafio_13/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace MyApplication {
+
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Some números até que seja digitado 0.");
+        }
+    }
+}
