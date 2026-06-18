8528b5f0fba4c700f33c656ed8db86868d1d22df
jvdev2001 <joaovitoralvess2001@gmail.com>
2026-06-18 15:50:11 -0300

    Finalizando o Desafio_14

---
+++ Desafio_14/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace MyApplication
+{
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Leia N e mostre sua tabuada.\r\n");
+
+            int n , soma = 0;
+
+            Console.Write("Digite um numero para mostra a tabuada  :");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <=10; i++) {
+                soma = n + i;
+                Console.WriteLine($"{n} + {i} = {soma}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
